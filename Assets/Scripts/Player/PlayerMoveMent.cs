85c4bdc627fcea704d3c0681c66e0711e2de20c8
plgg <18811991672@163.com>
2021-05-16 20:15:43 +0800

    武器不旋转的扔出收回实现

--- Assets/Scripts/Player/PlayerMoveMent.cs
+++ Assets/Scripts/Player/PlayerMoveMent.cs
@@ -3,20 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.UIElements;
 using UnityEngine;
+using Weapon;
 
 public class PlayerMoveMent : MonoBehaviour
 {
     // Start is called before the first frame update
     private float moveH;
     private float moveV;
-    private Rigidbody2D _rigidbody2D;
+    private Rigidbody2D _rigidbody;
     public float Speed;
     private Vector2 moveDirection;
     private Animator _animator;
+    public Transform WeaponPosition;
+    public Transform Destination;
+
+    private bool weaponOnhand;
+    public event Action<Vector3> FireWeapon;
+    public event Action<Vector3> CallBack;
+    public event Action<Vector3> PlayerMove; 
+
+    private WeaponController weapon;
     void Start()
     {
         _animator = GetComponent<Animator>();
-        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        weaponOnhand = true;
+        weapon = GetComponentInChildren<WeaponController>();
+        weapon.WeaponBack += handleWeaponBack;
     }
 
     // Update is called once per frame
@@ -25,6 +38,9 @@ public class PlayerMoveMent : MonoBehaviour
         moveInput();
         faceDirection();
         switchAnim();
+        weaponTest();
+        if(!weaponOnhand)
+            PlayerMove?.Invoke(WeaponPosition.position);
     }
 
     private void FixedUpdate()
@@ -43,21 +59,21 @@ public class PlayerMoveMent : MonoBehaviour
     //人物移动
     private void moveMent()
     {
-        _rigidbody2D.velocity = moveDirection;
+        _rigidbody.velocity = moveDirection;
     }
     //人物转向
     private void faceDirection()
     {
-        if(_rigidbody2D.velocity.x < 0f)
+        if(_rigidbody.velocity.x < 0f)
             transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
-        else if(_rigidbody2D.velocity.x > 0f)
+        else if(_rigidbody.velocity.x > 0f)
             transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
     }
 
     //动画切换
     private void switchAnim()
     {
-        if (Mathf.Abs(_rigidbody2D.velocity.x) > 0.1f)
+        if (Mathf.Abs(_rigidbody.velocity.x) > 0.1f)
         {
             _animator.SetBool("IsRun",true);
         }
@@ -65,5 +81,24 @@ public class PlayerMoveMent : MonoBehaviour
         {
             _animator.SetBool("IsRun",false);
         }
+    }
+
+    private void weaponTest()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FireWeapon?.Invoke(Destination.position);
+            weaponOnhand = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            CallBack?.Invoke(WeaponPosition.position);
+        }
+    }
+
+    private void handleWeaponBack()
+    {
+        weaponOnhand = true;
     }
 }
---
+++ Assets/Scripts/Weapon/WeaponController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using UnityEngine;
+
+namespace Weapon
+{
+    public class WeaponController : MonoBehaviour
+    {
+        [Header("状态参数")] 
+        public bool IsOut;
+        public bool IsCallBack;
+        public bool IsOnGround;
+        public bool IsInHand;
+
+        [Header("移动速度")] public float MoveSpeed;
+        [Header("旋转速度")] public float RotationSpeed;
+
+        private Vector2 moveVelocity;
+        private Vector3 outDestination;
+        private Vector3 backDestination;
+        private PlayerMoveMent _playerMoveMent;
+        private Quaternion originRotation;
+        private Rigidbody2D _rigidbody2D;
+
+        public event Action WeaponBack;  
+
+        private void Start()
+        {
+            IsCallBack = false;
+            IsOnGround = false;
+            IsOut = false;
+            IsInHand = true;
+            _playerMoveMent = GetComponentInParent<PlayerMoveMent>();
+            originRotation = transform.localRotation;
+            //注册发射与召回武器事件
+            _playerMoveMent.FireWeapon += handleFireWeapon;
+            _playerMoveMent.CallBack += handleCallBack;
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+            _playerMoveMent.PlayerMove += handlePlayerMove;
+        }
+
+        private void Update()
+        {
+            moveCalculation();
+            inHandTest();
+        }
+
+        private void FixedUpdate()
+        {
+            move();
+        }
+
+        private void moveCalculation()
+        {
+            if (IsCallBack)
+            {
+                if (Vector3.Distance(backDestination, transform.position) < 0.1f)
+                {
+                    IsCallBack = false;
+                    IsInHand = true;
+                    moveVelocity = Vector2.zero;
+                }
+                else
+                {
+                    moveVelocity = MoveSpeed * Time.fixedDeltaTime * (backDestination - transform.position).normalized;
+                }
+            }
+            if (IsOut)
+            {
+                if(Vector3.Distance(outDestination, transform.position) < 0.1f)
+                {
+                    IsOut = false;
+                    IsOnGround = true;
+                    moveVelocity = Vector2.zero;
+                }
+                else
+                {
+                    moveVelocity = MoveSpeed * Time.fixedDeltaTime * (outDestination - transform.position).normalized;
+                }
+            }
+            if(IsInHand || IsOnGround)
+                moveVelocity = Vector2.zero;
+        }
+        private void handleFireWeapon(Vector3 _destination)
+        {
+            if (IsInHand && !IsOnGround && !IsOut && !IsCallBack)
+            {
+                if(_destination != null) 
+                    outDestination = _destination;
+                IsInHand = false;
+                IsOut = true;
+            }
+        }
+        
+        private void handleCallBack(Vector3 _destination)
+        {
+            if (IsOut)
+            {
+                backDestination = _destination;
+                IsOut = false;
+                IsCallBack = true;
+            }
+            else if(IsOnGround)
+            {
+                if(_destination != null) 
+                    backDestination = _destination;
+                IsOnGround = false;
+                IsCallBack = true;
+            }
+        }
+
+        private void handlePlayerMove(Vector3 _destination)
+        {
+            backDestination = _destination;
+        }
+
+        private void move()
+        {
+            _rigidbody2D.velocity = moveVelocity;
+        }
+
+        private void inHandTest()
+        {
+            //如果在手里设置刚体无效跟着玩家位置移动，否则刚体有效，脱离父物体
+            if (IsInHand)
+            {
+                transform.SetParent(_playerMoveMent.transform);
+                _rigidbody2D.simulated = false;
+            }
+            else
+            {
+                transform.SetParent(null);
+                _rigidbody2D.simulated = true;
+            }
+        }
+
+        private void rotation()
+        {
+            if (IsOut)
+                transform.localRotation = new Quaternion(transform.localRotation.x, transform.localRotation.y,
+                    transform.localRotation.z + RotationSpeed,0f);
+            if(IsCallBack)
+                transform.localRotation = new Quaternion(transform.localRotation.x, transform.localRotation.y,
+                    transform.localRotation.z + RotationSpeed,0f);
+        }
+    }
+}
