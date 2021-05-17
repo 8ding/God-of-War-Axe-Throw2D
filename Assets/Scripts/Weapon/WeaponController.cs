f31d5f11e551e9b017e72ab5312dbb513e7d2be1
Poly <18811991672@163.com>
2021-05-17 23:15:38 +0800

    去刚体版

--- Assets/Scripts/Weapon/WeaponController.cs
+++ Assets/Scripts/Weapon/WeaponController.cs
@@ -18,7 +18,6 @@ namespace Weapon
         private Vector3 outDestination;
         private PlayerMoveMent _playerMoveMent;
         private Quaternion originRotation;
-        private Rigidbody2D _rigidbody2D;
         private Transform backDestination;
 
 
@@ -29,8 +28,7 @@ namespace Weapon
             originRotation = transform.localRotation;
             //注册发射与召回武器事件,玩家移动事件
             _playerMoveMent = GetComponentInParent<PlayerMoveMent>();
-            _rigidbody2D = GetComponent<Rigidbody2D>();
-            
+
             _playerMoveMent.FireWeapon += handleFireWeapon;
             _playerMoveMent.CallBack += handleCallBack;
             backDestination = _playerMoveMent.WeaponPosition;
@@ -43,41 +41,29 @@ namespace Weapon
             moveCalculation();
             rotation();
         }
-
-        private void FixedUpdate()
-        {
-            move();
-        }
+        
         
         private void moveCalculation()
         {
             if (IsCallBack)
             {
+                transform.position = Vector3.MoveTowards(transform.position, backDestination.position, MoveSpeed * Time.deltaTime);
                 if (Vector3.Distance(backDestination.position, transform.position) < 0.1f)
                 {
                     setInHand();
                     transform.position = backDestination.position;
                 }
-                else
-                {
-                    moveVelocity = MoveSpeed * Time.fixedDeltaTime * (backDestination.position - transform.position).normalized;
-                }
+
             }
             if (IsOut)
             {
-                if(Vector3.Distance(outDestination, transform.position) < 0.2f)
+                transform.position = Vector3.MoveTowards(transform.position, outDestination, MoveSpeed * Time.deltaTime);
+                if(Vector3.Distance(outDestination, transform.position) < 0.1f)
                 {
                     IsOut = false;
                     IsOnGround = true;
-                    moveVelocity = Vector2.zero;
-                }
-                else
-                {
-                    moveVelocity = MoveSpeed * Time.fixedDeltaTime * (outDestination - transform.position).normalized;
                 }
             }
-            if(IsInHand || IsOnGround)
-                moveVelocity = Vector2.zero;
         }
         private void handleFireWeapon(Vector3 _destination)
         {
@@ -105,15 +91,6 @@ namespace Weapon
             }
         }
 
-//        private void handlePlayerMove(Vector3 _destination)
-//        {
-//            backDestination = _destination;
-//        }
-
-        private void move()
-        {
-            _rigidbody2D.velocity = moveVelocity;
-        }
 
         private void rotation()
         {
@@ -123,24 +100,21 @@ namespace Weapon
                 transform.Rotate(0,0,-10 * RotationSpeed * Time.deltaTime);
                 
         }
-        //设置武器在手中，以角色为父物体，刚体失效
+        //设置武器在手中，以角色为父物体,设置为初始旋转度
         private void setInHand()
         {
             IsCallBack = false;
             IsOnGround = false;
             IsCallBack = false;
             IsInHand = true;
-
-            moveVelocity = Vector2.zero;
+            
             transform.SetParent(_playerMoveMent.transform);
             transform.localRotation = originRotation;
-            _rigidbody2D.simulated = false;
         }
-        //设置武器在离开中，脱离父物体，刚体激活
+        //设置武器在离开中，脱离父物体
         private void setOutHand()
         {
             transform.SetParent(null);
-            _rigidbody2D.simulated = true;
         }
     }
 }
